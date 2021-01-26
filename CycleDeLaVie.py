a633c34fa51d0967723d4f62b40b9ab3b34a0f14
florent le maire <lemaireflorent9@gmail.com>
2021-01-26 22:11:43 +0100

    Ajout du crossover

--- CycleDeLaVie.py
+++ CycleDeLaVie.py
@@ -3,30 +3,66 @@ import string
 from Entity import Entity
 
 
+def tri_ins(t, j=1):
+    if j < len(t):
+        insere(t, j)
+        tri_ins(t, j + 1)
+
+
+def insere(t, j):
+    if j > 0 and t[j] < t[j - 1]:
+        t[j - 1], t[j] = t[j], t[j - 1]
+        insere(t, j - 1)
+
+
 class CycleDeLaVie:
     def __init__(self, pop, tmut, tsel, objectif):
         self._tauxDeMutation = tmut
         self._nombreDePop = pop
-        self._tauxDeSelection = tsel
+        self._tauxDeSelection = tsel  # en pourcentage
         self._objectif = objectif
         self.listDesEntity = []  # La liste en public pour faire des test il faut la mettre en privé
         self.construction_ecosystem()
 
-    @staticmethod
-    def selection():
-        print("tt")
+    def selection(self):
+        copy_des_entity = [];
+        for e in self.listDesEntity:
+            e.fitness(self._objectif)
 
-    @staticmethod
-    def crossover():
-        print("je fais le crossoveur")
+        copy_des_entity[:] = self.listDesEntity
+        self.tri_ins(copy_des_entity)
+
+        nombre_entity_garder = round( (len(self.listDesEntity) * self._tauxDeSelection)/100)
+
+        self.listDesEntity[:] = copy_des_entity[:nombre_entity_garder]
+
+    def tri_ins(self, t, j=1):
+        if j < len(t):
+            self.insere(t, j)
+            self.tri_ins(t, j + 1)
+
+    def insere(self, t, j):
+        if j > 0 and t[j].get_fitness() > t[j - 1].get_fitness():
+            t[j - 1], t[j] = t[j], t[j - 1]
+            self.insere(t, j - 1)
+
+    def crossover(self):
+        copyList = []
+        copyList[:] = self.listDesEntity
+        cut = round(len(self._objectif)/2)
+        for i in range(0,len(self.listDesEntity),2):
+            if i+1 < len(self.listDesEntity):
+                self.listDesEntity.append(Entity(copyList[i].get_gene()[:cut]+copyList[i].get_gene()[cut:]))
+                self.listDesEntity.remove(copyList[i])
+                self.listDesEntity.remove(copyList[i+1])
 
     @staticmethod
     def mutation():
         print("je fais la mutation")
 
-    @staticmethod
-    def go():
-        print("je jeu commence")
+    def go(self):
+        self.selection()
+        self.crossover()
 
     def construction_ecosystem(self):
         for i in range(self._nombreDePop):
@@ -37,7 +73,12 @@ class CycleDeLaVie:
         letters = string.printable
         return ''.join(random.choice(letters) for i in range(length))
 
+    def printList(self):
+        for e in self.listDesEntity:
+            print(e.to_string())
 
-m = CycleDeLaVie(10, 1, 1, "SALUT")
-for e in m.listDesEntity:
-    print(e.to_string())
+
+m = CycleDeLaVie(10, 1, 70, "SALUT")
+m.go()
+
+
--- Entity.py
+++ Entity.py
@@ -1,3 +1,4 @@
+import random
 
 
 class Entity:
@@ -6,16 +7,21 @@ class Entity:
         self._Efitness = 0
 
     def fitness(self,but):
-        for i in range(len(self._gene)):
-            if self._gene[i] == but[i]:
-                self._Efitness += 1
+        # for i in range(len(self._gene)):
+        #     if self._gene[i] == but[i]:
+        #         self._Efitness += 1
+        # self._Efitness /= len(self._gene)
+        self._Efitness = random.random()
 
 
     def get_fitness(self):
         return self._Efitness
 
+    def get_gene(self):
+        return self._gene
+
 
     def to_string(self):
-        return " gene = "+ self._gene+" \n"+" fitness = "+str(self._Efitness)
+        return "[ gene = "+ self._gene+" \n"+" fitness = "+str(self._Efitness)+"]"
 
 
